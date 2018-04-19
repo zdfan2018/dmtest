d928b0b215805b90335de51d70232c930cc938dc
zdfan2018 <darren@payplus.co.nz>
2018-04-20 08:41:21 +1200

    initial master

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+          <!-- Stylesheets -->
+          <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    </head>
+    <body>
+        <?php
+            echo "index file";
+        ?>
+    </body>
+</html>
